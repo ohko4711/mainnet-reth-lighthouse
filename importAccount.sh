2de571f9b1fca060157a84748b907988a11d18a1
lyfsn <dev.wangyu@proton.me>
2024-04-28 16:45:07 +0800

    import account

---
+++ importAccount.sh
@@ -0,0 +1,12 @@
+
+docker run \
+  --rm \
+  -it \
+  -v $(pwd)/consensus-data:/consensus-data \
+  -v $(pwd)/validator_keys:/validator_keys \
+  -v $(pwd)/../network_config:/network_config \
+  sigp/lighthouse:v5.1.3 \
+  --testnet-dir=/network_config \
+  account validator import \
+  --directory=/validator_keys
+
