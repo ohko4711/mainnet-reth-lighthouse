2e5fea99bef307817e01203e554ba328f1e004c6
lyfsn <dev.wangyu@proton.me>
2024-04-28 19:20:57 +0800

    add validator info by status

--- status.sh
+++ status.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Define API URL variables
-API_URL="http://localhost:8545"
-ETH2_API_URL="http://127.0.0.1:5052/eth/v1/node/syncing"
+# Common header for JSON-RPC requests
+COMMON_HEADER="Content-Type: application/json"
+
+# API token for accessing the validators
+API_TOKEN=$(cat "$(pwd)/validator_keys/api-token.txt")
 
 echo "Fetching the current block number from the Endurance mainnet..."
 # Send a JSON-RPC request to get the current block number
-response=$(curl -s -X POST \
-  -H "Content-Type: application/json" \
+response=$(curl -s -X POST -H "$COMMON_HEADER" \
   --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' \
-  "$API_URL")
+  "http://localhost:8545")
 
 # Display the response or error message
 echo "Response: $response"
@@ -17,10 +18,9 @@ echo "---"
 
 echo "Querying the current number of network peers..."
 # Send a request to get the network peer count
-peer_count=$(curl -s -X POST \
- -H "Content-Type: application/json" \
+peer_count=$(curl -s -X POST -H "$COMMON_HEADER" \
  --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' \
- "$API_URL")
+ "http://localhost:8545")
 
 # Display the response or error message
 echo "Response: $peer_count"
@@ -28,8 +28,19 @@ echo "---"
 
 echo "Checking the syncing status of Endurance 2.0 node..."
 # Use a GET request to query the ETH2 node syncing status
-sync_status=$(curl -s --location --request GET "$ETH2_API_URL")
+sync_status=$(curl -s --location --request GET \
+  "http://127.0.0.1:5052/eth/v1/node/syncing")
 
 # Display the response or error message
 echo "Response: $sync_status"
+echo "---"
+
+echo "Checking the validator status..."
+# Querying validator status with authorization
+validator_status=$(curl -s --location --request GET \
+  -H "Authorization: Bearer $API_TOKEN" \
+  "http://127.0.0.1:5062/lighthouse/validators")
+
+# Display the response or error message
+echo "Response: $validator_status"
 echo "---"
