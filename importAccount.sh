4d5aa1cb2112f5c8307cf768ba9474276be3436f
lyfsn <dev.wangyu@proton.me>
2024-04-29 11:57:40 +0800

    use latest docker

--- importAccount.sh
+++ importAccount.sh
@@ -5,6 +5,6 @@ docker run \
   -v $(pwd)/consensus-data:/consensus-data \
   -v $(pwd)/validator_keys:/validator_keys \
   -v $(pwd)/../network_config:/network_config \
-  sigp/lighthouse:v5.1.3 \
+  sigp/lighthouse:latest \
   lighthouse --testnet-dir=/network_config \
   account validator import --directory=/validator_keys
