879cf10c0a9bb6828ee89a6eaba66dbc01f47b82
lyfsn <dev.wangyu@proton.me>
2024-04-30 11:21:15 +0800

    status check

--- status.sh
+++ status.sh
@@ -1,46 +1,37 @@
 #!/bin/bash
 
-# Common header for JSON-RPC requests
 COMMON_HEADER="Content-Type: application/json"
-
-# API token for accessing the validators
 API_TOKEN=$(cat "$(pwd)/validator_keys/api-token.txt")
 
-echo "Fetching the current block number from the Endurance mainnet..."
-# Send a JSON-RPC request to get the current block number
+echo "Requesting current block number from the Endurance mainnet..."
 response=$(curl -s -X POST -H "$COMMON_HEADER" \
   --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' \
   "http://localhost:8545")
-
-# Display the response or error message
-echo "Response: $response"
+echo "Current Block Number: $response"
 echo "---"
 
-echo "Querying the current number of network peers..."
-# Send a request to get the network peer count
+echo "Requesting current Execution Layer node peer count..."
 peer_count=$(curl -s -X POST -H "$COMMON_HEADER" \
  --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' \
  "http://localhost:8545")
-
-# Display the response or error message
-echo "Response: $peer_count"
+echo "Execution Layer Peers: $peer_count"
 echo "---"
 
-echo "Checking the syncing status of Endurance 2.0 node..."
-# Use a GET request to query the ETH2 node syncing status
+echo "Requesting current Consensus Layer node peer count..."
+sync_status=$(curl -s --location --request GET \
+  "http://127.0.0.1:5052/eth/v1/node/peer_count")
+echo "Consensus Layer Peers: $sync_status"
+echo "---"
+
+echo "Requesting current syncing status of the Endurance 2.0 node..."
 sync_status=$(curl -s --location --request GET \
   "http://127.0.0.1:5052/eth/v1/node/syncing")
-
-# Display the response or error message
-echo "Response: $sync_status"
+echo "Syncing Status: $sync_status"
 echo "---"
 
-echo "Checking the validator status..."
-# Querying validator status with authorization
+echo "Requesting current validator status..."
 validator_status=$(curl -s --location --request GET \
   -H "Authorization: Bearer $API_TOKEN" \
   "http://127.0.0.1:5062/lighthouse/validators")
-
-# Display the response or error message
-echo "Response: $validator_status"
+echo "Validator Status: $validator_status"
 echo "---"
