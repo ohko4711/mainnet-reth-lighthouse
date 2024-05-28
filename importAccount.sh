e390a8306ff0aa4aa1210de3c99d08f7f331843a
lyfsn <dev.wangyu@proton.me>
2024-05-28 15:54:52 +0800

    fix importAccount script

--- importAccount.sh
+++ importAccount.sh
@@ -1,10 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <validator_keys_directory>"
+  exit 1
+fi
+
+VALIDATOR_KEYS_DIR=$1
 
 docker run \
   --rm \
   -it \
   -v $(pwd)/consensus-data:/consensus-data \
-  -v $(pwd)/validator_keys:/validator_keys \
+  -v $VALIDATOR_KEYS_DIR:/validator_keys \
   -v $(pwd)/../network_config:/network_config \
   sigp/lighthouse:latest \
-  lighthouse --testnet-dir=/network_config \
-  account validator import --directory=/validator_keys
+  lighthouse \
+  --testnet-dir=/network_config \
+  --datadir=/consensus-data \
+  account validator import \
+  --directory=/validator_keys
