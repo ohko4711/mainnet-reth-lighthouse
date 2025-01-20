ce3e1b473c127692a703f8a56bb86b2aca5e8740
richard <ohko4711@163.com>
2025-01-20 15:02:29 +0800

    optimize: stop.sh

--- stop.sh
+++ stop.sh
@@ -1,8 +1,12 @@
-# Check if MEV version is running
-if docker compose -f compose-mev-version.yaml ps | grep -q "Up"; then
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_NAME="$(basename "${SCRIPT_DIR}")"
+
+# Check if MEV boost service exists
+if docker ps | grep "${PROJECT_NAME}-boost-1"; then
     echo "Stopping MEV version..."
-    docker compose -f compose-mev-version.yaml stop
+    docker compose -f "${SCRIPT_DIR}/compose-mev-version.yaml" stop
 else
     echo "Stopping default version..."
-    docker compose -f compose.yaml stop
+    docker compose -f "${SCRIPT_DIR}/compose.yaml" stop
 fi
